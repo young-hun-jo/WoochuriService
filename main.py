7b7a2133b64c3e24d123e4956424ff2d2beeddf8
young-hun-jo <joyh951021@gmail.com>
2021-05-08 14:16:17 +0900

    2021-05-08일 매출 예측

--- main.py
+++ main.py
@@ -6,12 +6,12 @@ print('예측하려는 날짜:', pd.Timestamp.now())
 
 # Load updated crawling dataset and modeling to predict tomorrow's sale
 # Setting parameters using local MySQL id, password
-user, password = 'root', 'your_pw@'
+user, password = 'root', 'password@'
 end_time = (pd.Timestamp.now() - pd.Timedelta(days=1)).strftime("%Y-%m-%d")
 
 # run crawling, preprocess datasets, and finally prediction at one time
 PredModel = WoochuriPredModel(user=user, password=password, end_time=end_time,
-                              today_sale=655600, remark='평일')
+                              today_sale=67200, remark='평일')
 FinalDataset = PredModel.execute()
 result = PredModel.run(FinalDataset)
 
